7f62d5310bf19ae92a075f53551e86e0cbe11f03
Karunia Prima Aditya <newcyber.inc@gmail.com>
2026-08-01 00:20:58 +0700

    chore: initialize nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-5xl font-bold text-sky-700">
+        Fish Market Platform
+      </h1>
+    </main>
+  );
+}
