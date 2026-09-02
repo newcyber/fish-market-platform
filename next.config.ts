0840372c43c501dd78175e6160677e68f6d4b5ad
newcyber <newcyber.inc@gmail.com>
2026-09-03 02:16:24 +0700

    fix: externalize Prisma client in production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+
+  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
